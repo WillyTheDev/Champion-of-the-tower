82171eb085ad4bdd834956e3a48d1f706fb9ae66
William Scholder <54202351+WillyTheDev@users.noreply.github.com>
2021-07-11 11:10:16 +0200

    Enemy Movement improved
    - Almost perfect.
    - Enemy can now change direction based on multiple factor.
    - Enemy is now stopping on the side of the player and not on him.

--- Champion of the tower/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Champion of the tower/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -38,12 +39,11 @@ public class EnemyMovement : MonoBehaviour
 
     List<Vector3> PathFindingEnemy()
     {
-        enemyIsMoving = true;
-        
+        Debug.Log("Checking for Enemy best path");
         List<Vector3> xCellPaths = new List<Vector3>();
         List<Vector3> zCellPaths = new List<Vector3>();
-        
-        for (int i = 0; i <= enemyMovementPoint; i++)
+        //Creating a path going x Axis & a path going on Z axis
+        for (int i = 0; i < enemyMovementPoint; i++)
         {
             if(i == 0)
             {
@@ -56,25 +56,58 @@ public class EnemyMovement : MonoBehaviour
                 zCellPaths.Add(cellPath(zCellPaths[zCellPaths.Count - 1].x, zCellPaths[zCellPaths.Count - 1].z, "Forward"));
             }
             
-            Debug.Log("last xCell =" + xCellPaths[xCellPaths.Count - 1]);
         }
-        return Vector3.Distance(xCellPaths[xCellPaths.Count -1], PlayerMovement.playerPosition) > Vector3.Distance(zCellPaths[zCellPaths.Count - 1], PlayerMovement.playerPosition)  ? zCellPaths : xCellPaths;
+        Debug.Log("xPath :" + xCellPaths.Count);
+        Debug.Log("zPath :" + zCellPaths.Count);
 
+        // Removing dupplicate value cerated on the loop to have the smallest path
+        List<Vector3> uniqueXCellPath = xCellPaths.Distinct().ToList<Vector3>();
+        List<Vector3> uniqueZCellPath = zCellPaths.Distinct().ToList<Vector3>();
+        Debug.Log("uniqueXPath :" + uniqueXCellPath.Count);
+        Debug.Log("uniqueZPath :" + uniqueZCellPath.Count);
+        // Returning the path based last cell of the path and his distance with playerPositon.
+        return Vector3.Distance(uniqueXCellPath[uniqueXCellPath.Count -1], PlayerMovement.playerPosition) > Vector3.Distance(uniqueZCellPath[uniqueZCellPath.Count - 1], PlayerMovement.playerPosition)  ? zCellPaths : xCellPaths;
+
+
+        //this nested Function are checking if the cell can be used by the enemy
         Vector3 cellPath(float xPosition, float zPosition, string axis)
         {
-            int xOffsetWithPlayer = Convert.ToInt16( - transform.position.x);
-            int zOffsetWithPlayer = Convert.ToInt16(PlayerMovement.playerPosition.z - transform.position.z);
+            int xOffsetWithPlayer = Convert.ToInt16(PlayerMovement.playerPosition.x - xPosition);
+            int zOffsetWithPlayer = Convert.ToInt16(PlayerMovement.playerPosition.z - zPosition);
 
             bool xIsPositive = xOffsetWithPlayer > 0;
             bool zIsPositive = zOffsetWithPlayer > 0;
 
+
             RaycastHit hitForward;
             RaycastHit hitRight;
 
+            // this next block is checking if the cell is currently where it should be (next to the player)
+            Vector3 primaryTargetCell;
+            Vector3 secondaryTargetCell;
             Vector3 originPosition = new Vector3(xPosition, 2, zPosition);
-            Debug.Log("X is Positive :" + xIsPositive);
-            Debug.Log("Z is Positive :" + zIsPositive);
-            if (axis == "Right")
+
+            if (xOffsetWithPlayer != zOffsetWithPlayer)
+            {
+                primaryTargetCell = PlayerMovement.playerPosition + new Vector3(xOffsetWithPlayer > zOffsetWithPlayer?(xIsPositive ? -5 : 5) : 0, 0,zOffsetWithPlayer > xOffsetWithPlayer ? (zIsPositive ? -5 : 5) : 0);
+                secondaryTargetCell = PlayerMovement.playerPosition + new Vector3(zOffsetWithPlayer > xOffsetWithPlayer ? (xIsPositive ? -5 : 5) : 0, 0, xOffsetWithPlayer > zOffsetWithPlayer ? (zIsPositive ? -5 : 5) : 0);
+            } else
+            {
+                primaryTargetCell = PlayerMovement.playerPosition + new Vector3(xIsPositive ? -5 : 5, 0, 0);
+                secondaryTargetCell = PlayerMovement.playerPosition + new Vector3(0, 0, zIsPositive ? -5 : 5);
+            }
+
+            // if the cell is on the targetCell (primary or secondary, we will return his position)
+            if (originPosition.x == primaryTargetCell.x && originPosition.z == primaryTargetCell.z)
+            {
+                return originPosition;
+            }
+            else if (originPosition.x == secondaryTargetCell.x && originPosition.z == secondaryTargetCell.z)
+            {
+                return originPosition;
+            }
+
+            if (axis == "Right" && xPosition != PlayerMovement.playerPosition.x)
             {
                 if (Physics.Raycast(originPosition,xIsPositive ? Vector3.right : Vector3.left, out hitRight, 5))
                 {
@@ -84,7 +117,7 @@ public class EnemyMovement : MonoBehaviour
                 {
                     return new Vector3(originPosition.x + 5 * (xIsPositive ? 1 : -1), originPosition.y, originPosition.z);
                 }
-            } else
+            } else if (zPosition != PlayerMovement.playerPosition.z)
             {
                 if (Physics.Raycast(originPosition,zIsPositive ? Vector3.forward : Vector3.back, out hitForward, 5))
                 {
@@ -94,6 +127,9 @@ public class EnemyMovement : MonoBehaviour
                 {
                     return new Vector3(originPosition.x, originPosition.y, originPosition.z + 5 * (zIsPositive ? 1 : -1));
                 }
+            } else
+            {
+                return new Vector3(originPosition.x + 5 * (xIsPositive ? 1 : -1), originPosition.y, originPosition.z);
             }
 
         }
@@ -102,6 +138,7 @@ public class EnemyMovement : MonoBehaviour
 
     IEnumerator MoveEnemyBasedOnPath(List<Vector3> path)
     {
+        Debug.Log("Moving enemies on the path...");
         foreach (Vector3 cell in path)
         {
             for(float ft = 0f; ft <= 1; ft += Time.fixedDeltaTime)
@@ -113,6 +150,7 @@ public class EnemyMovement : MonoBehaviour
         }
         enemyMovementPoint = 0;
         enemyIsMoving = false;
+        Debug.Log("Enemy isn't moving anymore");
     }
 
 
@@ -120,162 +158,7 @@ public class EnemyMovement : MonoBehaviour
 
 
 
-private void DefineTargetCellForEnemyMovement()
-    {
-        RaycastHit hitForward;
-        RaycastHit hitRight;
-        double movementPointDivided = enemyMovementPoint / 2;
 
-        Debug.DrawLine(transform.position, PlayerMovement.playerPosition, Color.cyan);
-        // we have for X and Z the distance between enemy and Player.
-        xOffset = Convert.ToInt16(PlayerMovement.playerPosition.x - transform.position.x);
-        zOffset = Convert.ToInt16(PlayerMovement.playerPosition.z - transform.position.z);
-        // We are defining wich is the best cell to target based on the offset
-        // If we are align on the x axis, then we will go on a straight line
-        if (xOffset == 0)
-        {
-            Debug.Log("Enemy is supposed to go forward");
-            xPriority = false;
-            Debug.DrawRay(transform.position, xOffset > 0 ? Vector3.right : Vector3.left * (5 * enemyMovementPoint), Color.red);
-
-            
-                if (Physics.Raycast(transform.position, zOffset > 0 ? Vector3.forward : Vector3.back, out hitForward, 5 * enemyMovementPoint))
-                {
-                
-                    if (hitForward.collider.CompareTag("Obstacle"))
-                    {
-                        Debug.Log("Enemy met an obstacle ! on Z Axis");
-                        calculatePositionWhenNotAlign();
-                    }
-                    else
-                    {
-                        Debug.Log("There is no obstacle");
-                        targetPosition = enemyInitialPosition + new Vector3(0, 0, 5 * enemyMovementPoint * (zOffset > 0 ? 1 : -1));
-                    }
-
-                
-            }
-            else
-            {
-                Debug.Log("Weird");
-            }
-            
-        }
-        // If we are align on the z axis, then we will go on a straight line
-        else if (zOffset == 0)
-        {
-            xPriority = true;
-            Debug.DrawRay(transform.position, transform.forward * (zOffset > 0 ? 20 : -20), Color.green);
-            if (Physics.Raycast(transform.position, xOffset > 0 ? Vector3.right : Vector3.left, out hitRight, 5 * enemyMovementPoint))
-                
-            {
-                if (hitRight.collider.CompareTag("Obstacle"))
-                {
-                    Debug.Log("Enemy met an obstacle ! on X Axis");
-                    calculatePositionWhenNotAlign();
-                } else
-                {
-                    targetPosition = enemyInitialPosition + new Vector3(5 * enemyMovementPoint * (xOffset > 0 ? 1 : -1), 0, 0);
-                }
-            } 
-            
-        }
-        // If the player is not align vertically or horizontally
-        // We mush check for is movementPoint and attribute them base on distance on both axis.
-        else
-        {
-            calculatePositionWhenNotAlign();
-        }
-
-        void calculatePositionWhenNotAlign()
-        {
-            if (enemyMovementPoint % 2 != 0)
-            {
-                // if the distance on x Axis is longer than the z, then the enemy will move more on the longer axis.
-                xPriority = (xOffset * (xOffset > 0 ? 1 : -1)) > (zOffset * (zOffset > 0 ? 1 : -1));
-
-                float xPosition = Convert.ToSingle(5 * (xPriority ? Math.Ceiling(movementPointDivided) : Math.Floor(movementPointDivided)) * (xOffset > 0 ? 1 : -1));
-                float zPosition = Convert.ToSingle(5 * (xPriority ? Math.Floor(movementPointDivided) : Math.Ceiling(movementPointDivided)) * (zOffset > 0 ? 1 : -1));
-
-                targetPosition = enemyInitialPosition + new Vector3(xPosition, 0, zPosition);
-            }
-            else
-            {
-                targetPosition = enemyInitialPosition + new Vector3(5 * (enemyMovementPoint / 2) * (xOffset > 0 ? 1 : -1), 0, 5 * (enemyMovementPoint / 2) * (zOffset > 0 ? 1 : -1));
-            }
-        }
-        // we were checking if there were obstacle on the path we defined, and prioritize an axis for avoiding this obstacle.
-        
-        if (Physics.Raycast(transform.position, zOffset > 0 ? Vector3.forward : Vector3.back, out hitForward, 5 * enemyMovementPoint))
-        {
-            if (hitForward.collider.CompareTag("Obstacle"))
-            {
-                Debug.Log("Obstacle on the forward way !");
-                directionShouldBeRight = true;
-            }
-        }
-        if (Physics.Raycast(transform.position, xOffset > 0 ? Vector3.right : Vector3.left, out hitRight, 5 * enemyMovementPoint))
-        {
-            if (hitRight.collider.CompareTag("Obstacle"))
-            {
-                Debug.Log("Obstacle on The Right way");
-                directionShouldBeRight = false;
-            }
-        }
-        // Once everything is define we can move the enemy
-        enemyIsMoving = true;
-        enemyMovementPoint = 0;
-    }
-
-
-
-    private void MovePlayerToSelectedCell(int xOffset, int zOffset)
-    {
-        
-        Debug.Log("targetPosition :" + targetPosition);
-        Debug.Log("Enemy Initial Position" + enemyInitialPosition);
-
-        if (directionShouldBeRight)
-        {
-            if (xOffset > 0 ? transform.position.x < targetPosition.x : transform.position.x > targetPosition.x)
-            {
-                transform.Translate(Vector3.right * Time.deltaTime * speed * (xOffset > 0 ? 1 : -1));
-            }
-            else if (zOffset > 0 ? transform.position.z < targetPosition.z : transform.position.z > targetPosition.z)
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime * speed * (zOffset > 0 ? 1 : -1));
-            }
-            else
-            {
-                // Center the Player position in the Cell and Stopping the movement.
-                enemyMovementPoint = 0;
-                transform.position = targetPosition;
-                enemyInitialPosition = transform.position;
-                enemyIsMoving = false;
-            }
-        }
-        else
-        {
-            if (zOffset > 0 ? transform.position.z < targetPosition.z : transform.position.z > targetPosition.z)
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime * speed * (zOffset > 0 ? 1 : -1));
-            }
-            else if (xOffset > 0 ? transform.position.x < targetPosition.x : transform.position.x > targetPosition.x)
-            {
-                transform.Translate(Vector3.right * Time.deltaTime * speed * (xOffset > 0 ? 1 : -1));
-            }
-            else
-            {
-                // Center the Player position in the Cell and Stopping the movement.
-                enemyMovementPoint = 0;
-                transform.position = targetPosition;
-                enemyInitialPosition = transform.position;
-                enemyIsMoving = false;
-            }
-        }
-
-
-    }
 
 
 }
